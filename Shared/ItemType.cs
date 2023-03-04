58b30fc2e1e8d0fb2073d86b3d5c079d79fed897
jgcodes2020 <jackyguo18@hotmail.com>
2023-03-03 22:09:40 -0500

    Basic DB/serializer types

---
+++ Shared/ItemType.cs
@@ -0,0 +1,18 @@
+namespace Yrhacks2023.Shared;
+
+public enum ItemType
+{
+    Cpu = 0,
+    Gpu = 1,
+    Ram = 2,
+    WaifuBodyPillow = 3,
+    Motherboard = 4,
+    Case = 5,
+    Hdd = 6,
+    Ssd = 7,
+    Mouse = 8,
+    Keyboard = 9,
+    Monitor = 10,
+    Cooler = 11,
+    CaseFan = 12
+}
---
+++ Shared/Product.cs
@@ -0,0 +1,10 @@
+namespace Yrhacks2023.Shared;
+
+public class Product
+{
+    public string Name { get; set; } = string.Empty;
+    public int Type { get; set; }
+    public string Description { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+    public string Seller { get; set; } = string.Empty;
+}
